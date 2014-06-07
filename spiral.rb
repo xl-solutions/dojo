83fda53e0adc4064d67bef7411b9dc83d5906c32
Estevão de Abreu Machado <estevaom@gmail.com>
2014-06-07 14:33:01 -0300

    creates draw method

--- spiral.rb
+++ spiral.rb
@@ -9,17 +9,10 @@ class Spiral
     @max = x*y
 
     @the_great_array = Array.new(y)
-
     @the_great_array.map! { |i| Array.new(x) }
 
     run(@rows, @cols, @ring, @counter, @max, @the_great_array)
-
-    @the_great_array.each_with_index do |row, i|
-      row.each do |num|
-        print "#{num}" + " "*(5-num.to_s.size.to_i)
-      end
-      print "\n"
-    end
+    draw(@the_great_array)
   end
 
   def run(rows, cols, ring, counter, max, the_great_array)
@@ -28,17 +21,15 @@ class Spiral
     (ring..((rows-1)-ring)).each do |i|
       the_great_array[ring][i] = counter
       counter += 1
-    return if counter > max
+      return if counter > max
     end
-
 
     # Linha vertical direita (desce)
-    ((0+1+ring)..((cols-2)-ring)).each do |i|
+    ((ring+1)..((cols-2)-ring)).each do |i|
       the_great_array[i][((rows-1)-ring)] = counter
       counter += 1
-    return if counter > max
+      return if counter > max
     end
-
 
     # Linha horizontal inferior (volta)
     r = (((rows-1)-ring)..ring)
@@ -46,9 +37,8 @@ class Spiral
     (r.first).downto(r.last).each do |i|
       the_great_array[((cols-1)-ring)][i] = counter
       counter += 1
-    return if counter > max
+      return if counter > max
     end
-
 
     # Linha vertical esquerda (sobe)
     r = (((cols-2)-ring)..(ring+1))
@@ -56,14 +46,22 @@ class Spiral
     (r.first).downto(r.last).each do |i|
       the_great_array[i][ring] = counter
       counter += 1
-    return if counter > max
+      return if counter > max
     end
 
-
     ring += 1
-
+    
     run(rows, cols, ring, counter, max, the_great_array)
+  end
+
+  def draw(the_great_array)
+    the_great_array.each do |row|
+      row.each do |num|
+        print "#{num}" + " "*(5-num.to_s.size.to_i)
+      end
+      print "\n"
+    end
   end
 end
 
-Spiral.new(7,5)
+Spiral.new(10,10)
