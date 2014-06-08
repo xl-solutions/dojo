d307ffda7b88530db33ea1045b81fd6ffa2e43a1
Estevão de Abreu Machado <estevaom@gmail.com>
2014-06-08 15:50:24 -0300

    Removes useless parameters :P

--- 2014-06-06/users/estevaom.rb
+++ 2014-06-06/users/estevaom.rb
@@ -1,61 +1,48 @@
-# Algoritmo Dojo 06/06/14
-
 class Spiral
   def initialize(x,y)
-    @rows = x
-    @cols = y
-    @ring = 0
-    @counter = 1
-    @max = x*y
-
-    @the_great_array = Array.new(y)
-    @the_great_array.map! { |i| Array.new(x) }
-
-    run(@rows, @cols, @ring, @counter, @max, @the_great_array)
-    draw(@the_great_array)
+    @rows            = x
+    @cols            = y
+    @ring            = 0
+    @max             = x*y
+    @counter         = 1
+    @the_great_array = Array.new(@cols) { Array.new(@rows) }
   end
 
-  def run(rows, cols, ring, counter, max, the_great_array)
-    
-    # Linha horizontal superior (vai)
-    (ring..((rows-1)-ring)).each do |i|
-      the_great_array[ring][i] = counter
-      counter += 1
-      return if counter > max
+  def run
+    (@ring..((@rows-1)-@ring)).each do |i|
+      @the_great_array[@ring][i] = @counter
+      @counter += 1
+      return if @counter > @max
     end
 
-    # Linha vertical direita (desce)
-    ((ring+1)..((cols-2)-ring)).each do |i|
-      the_great_array[i][((rows-1)-ring)] = counter
-      counter += 1
-      return if counter > max
+    ((@ring+1)..((@cols-2)-@ring)).each do |i|
+      @the_great_array[i][((@rows-1)-@ring)] = @counter
+      @counter += 1
+      return if @counter > @max
     end
 
-    # Linha horizontal inferior (volta)
-    r = (((rows-1)-ring)..ring)
-
+    r = (((@rows-1)-@ring)..@ring)
     (r.first).downto(r.last).each do |i|
-      the_great_array[((cols-1)-ring)][i] = counter
-      counter += 1
-      return if counter > max
+      @the_great_array[((@cols-1)-@ring)][i] = @counter
+      @counter += 1
+      return if @counter > @max
     end
 
-    # Linha vertical esquerda (sobe)
-    r = (((cols-2)-ring)..(ring+1))
-
+    r = (((@cols-2)-@ring)..(@ring+1))
     (r.first).downto(r.last).each do |i|
-      the_great_array[i][ring] = counter
-      counter += 1
-      return if counter > max
+      @the_great_array[i][@ring] = @counter
+      @counter += 1
+      return if @counter > @max
     end
 
-    ring += 1
+    @ring += 1
     
-    run(rows, cols, ring, counter, max, the_great_array)
+    run
   end
 
-  def draw(the_great_array)
-    the_great_array.each do |row|
+  def draw
+    run
+    @the_great_array.each do |row|
       row.each do |num|
         print "#{num}" + " "*(5-num.to_s.size.to_i)
       end
@@ -63,5 +50,3 @@ class Spiral
     end
   end
 end
-
-Spiral.new(10,10)
